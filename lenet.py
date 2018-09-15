1402315246e0949a3c6689b03de33979b0b010c0
Rafael Vareto <rafaelvareto@gmail.com>
2018-09-15 11:27:12 -0300

    Replacing skimage dataset with keras dataset

--- lenet.py
+++ lenet.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential
-from keras.layers.convolutional import Convolution2D
-from keras.layers.convolutional import MaxPooling2D
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
@@ -12,12 +12,12 @@ class LeNet:
         model = Sequential()
 
         # first set of CONV => RELU => POOL
-        model.add(Convolution2D(20, 5, 5, border_mode="same", input_shape=(depth, height, width)))
+        model.add(Conv2D(20, kernel_size=(5, 5), padding="same", input_shape=(width, height, depth)))
         model.add(Activation("relu"))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
         # second set of CONV => RELU => POOL
-        model.add(Convolution2D(50, 5, 5, border_mode="same"))
+        model.add(Conv2D(50, kernel_size=(5, 5), padding="same"))
         model.add(Activation("relu"))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
--- mnist.py
+++ mnist.py
@@ -1,45 +1,46 @@
 from lenet import LeNet
 from sklearn.cross_validation import train_test_split
-from sklearn import datasets
+from keras.datasets import mnist as keras_mnist
 from keras.optimizers import SGD
-from keras.utils import np_utils
+from keras.utils import np_utils as keras_np_utils
 import numpy as np
 import cv2
 
-weightsPath = "weights/lenet_weights.hdf5"
+# define some parameters
+img_rows = 28
+img_cols = 28
+num_classes = 10
+weightsPath = None#"weights/lenet_weights.hdf5"
 
 print("downloading MNIST...")
-dataset = datasets.fetch_mldata("MNIST Original")
+# load/download mnist dataset
+(trainX, trainY), (testX, testY) = keras_mnist.load_data()
 
-# reshape the MNIST dataset from a flat list of 784-dim vectors, to
-# 28 x 28 pixel images, then scale the data to the range [0, 1.0]
-# and construct the training and testing splits
-data = dataset.data.reshape((dataset.data.shape[0], 28, 28))
-data = data[:, np.newaxis, :, :]
-(trainData, testData, trainLabels, testLabels) = train_test_split(
-	data / 255.0, dataset.target.astype("int"), test_size=0.33)
+# adapting images to tensorflow backend (channels_last)
+x_train = trainX.reshape(trainX.shape[0], img_rows, img_cols, 1)
+x_test = testX.reshape(testX.shape[0], img_rows, img_cols, 1)
 
-# transform the training and testing labels into vectors in the
-# range [0, classes] -- this generates a vector for each label,
-# where the index of the label is set to `1` and all other entries
-# to `0`; in the case of MNIST, there are 10 class labels
-trainLabels = np_utils.to_categorical(trainLabels, 10)
-testLabels = np_utils.to_categorical(testLabels, 10)
+# convert target vectors to binary class matrices
+y_train = keras_np_utils.to_categorical(trainY, num_classes)
+y_test = keras_np_utils.to_categorical(testY, num_classes)
+
+
+print(x_train[0].shape)
 
 # initialize the optimizer and model
 print("[INFO] compiling model...")
 opt = SGD(lr=0.01)
-model = LeNet.build(width=28, height=28, depth=1, classes=10, weightsPath=weightsPath)
+model = LeNet.build(width=img_cols, height=img_rows, depth=1, classes=num_classes, weightsPath=weightsPath)
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 # if no weights specified train the model
 if weightsPath is None:
 	print("[INFO] training...")
-	model.fit(trainData, trainLabels, batch_size=128, nb_epoch=20, verbose=1)
+	model.fit(x_train, y_train, batch_size=128, epochs=20, verbose=1)
 
 	# show the accuracy on the testing set
 	print("[INFO] evaluating...")
-	(loss, accuracy) = model.evaluate(testData, testLabels, batch_size=128, verbose=1)
+	(loss, accuracy) = model.evaluate(x_test, y_test, batch_size=128, verbose=1)
 	print("[INFO] accuracy: {:.2f}%".format(accuracy * 100))
 
 	print("[INFO] dumping weights to file...")
